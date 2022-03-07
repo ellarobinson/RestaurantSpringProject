6024238b9d32f80ace066c9f07e5977639cae1fc
Ella Robinson <ellarobinson172@gmail.com>
2022-03-07 13:13:55 +0000

    Added create functionality to the server and controller and connected to MySQL database

--- src/main/java/com/qa/controller/Controller.java
+++ src/main/java/com/qa/controller/Controller.java
@@ -1,5 +1,29 @@
 package com.qa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.entity.Customer;
+import com.qa.service.CustomerService;
+
+@RestController
 public class Controller {
+	
+	private CustomerService service;
+
+	@Autowired
+	public Controller(CustomerService service) {
+		super();
+		this.service = service;
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+		return new ResponseEntity<Customer>(this.service.createCustomer(customer), HttpStatus.CREATED);
+	}
 
 }
